d8781b6a47e71bdadb9f29193eb630e19d299b19
Alexander Herrfurth <alexander.herrfurth@outlook.de>
2022-04-04 22:02:38 +0200

    Implemented DTOs for TwitchNxExtensionClient; implemented SetupCommands and ReceiveCommands methods in extension client;

---
+++ Yetibyte.Twitch.TwitchNx.ExtensionCommandSource/Client/DataTransfer/DataTransferObjects.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yetibyte.Twitch.TwitchNx.ExtensionCommandSource.Client.DataTransfer
+{
+    [Serializable]
+    public record Command(string Name, string Description, string CooldownGroup);
+
+    [Serializable]
+    public record CooldownGroup(string Name, double SharedTime);
+
+    [Serializable]
+    public record CommandRequest(string TwitchNxClientId, string MessageType, Command[] Commands, CooldownGroup[] CooldownGroups);
+
+    [Serializable]
+    public record SetupCommandsResponse(int CommandCount, int CooldownCount);
+
+    [Serializable]
+    public record UserCommand(string Name, string User, DateTime Time);
+
+    [Serializable]
+    public record ReceiveUserCommandsResponse(UserCommand[] Commands);
+
+}
--- Yetibyte.Twitch.TwitchNx.ExtensionCommandSource/Client/TwitchNxExtensionClient.cs
+++ Yetibyte.Twitch.TwitchNx.ExtensionCommandSource/Client/TwitchNxExtensionClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,47 +13,77 @@ namespace Yetibyte.Twitch.TwitchNx.ExtensionCommandSource.Client
 {
     public class TwitchNxExtensionClient
     {
-        private const string MESSAGE_TYPE_GET_COMMANDS = "getcmds";
+        private const string MESSAGE_TYPE_SETUP_COMMANDS= "setup_commands";
+        private const string MESSAGE_TYPE_RECEIVE_USER_COMMANDS = "receive_user_commands";
+
+        private static readonly SetupCommandsResponse EMPTY_SETUP_COMMANDS_RESPONSE = new SetupCommandsResponse(0, 0);
+        private static readonly ReceiveUserCommandsResponse EMPTY_RECEIVE_USER_COMMANDS_RESPONSE = new ReceiveUserCommandsResponse(Array.Empty<UserCommand>());
 
         private readonly RestClient _restClient;
-        private readonly string _userUrl;
 
-        public TwitchNxExtensionClient(string userUrl)
+        [DebuggerDisplay("****")]
+        private readonly string _twitchNxClientId;
+
+        public string BaseUrl { get; }
+
+        public TwitchNxExtensionClient(string twitchNxClientId, string baseUrl)
         {
-            _restClient = new RestClient();
+            _twitchNxClientId = twitchNxClientId;
+            BaseUrl = baseUrl;
+
+            _restClient = new RestClient(BaseUrl);
             _restClient.UseSystemTextJson(new System.Text.Json.JsonSerializerOptions
             {
-                PropertyNameCaseInsensitive = true,
+                PropertyNameCaseInsensitive = true
             });
-            _userUrl = userUrl;
         }
 
-        public IEnumerable<Command> FetchCommands(string channel)
+        public SetupCommandsResponse SetupCommands(IEnumerable<Command> commands, IEnumerable<CooldownGroup> cooldownGroups)
         {
-            CommandRequest commandRequest = new CommandRequest
-            {
-                TwitchStreamer = channel,
-                MessageType = MESSAGE_TYPE_GET_COMMANDS
-            };
+            CommandRequest commandRequest = new CommandRequest(
+                _twitchNxClientId,
+                MESSAGE_TYPE_SETUP_COMMANDS,
+                commands.ToArray(),
+                cooldownGroups.ToArray()
+            );
 
-            RestRequest restRequest = new RestRequest(_userUrl, Method.Post);
+            RestRequest restRequest = new RestRequest(string.Empty, Method.Post);
             restRequest.AddJsonBody(commandRequest);
 
-            Task<Command[]?>? fetchCommandsTask = _restClient.PostAsync<Command[]?>(restRequest);
+            Task<SetupCommandsResponse?> setupCommandsTask = _restClient.PostAsync<SetupCommandsResponse?>(restRequest);
 
-            if (fetchCommandsTask is not null)
-            {
-                fetchCommandsTask.Wait();
+            if (setupCommandsTask is null)
+                return EMPTY_SETUP_COMMANDS_RESPONSE;
 
-                IEnumerable<Command>? commands = fetchCommandsTask.Result;
+            setupCommandsTask.Wait(); // Enforce synchronous behavior
 
-                return commands ?? Array.Empty<Command>();
-            }
-            else
-            {
-                return Array.Empty<Command>();  
-            }
-            
+            SetupCommandsResponse setupCommandsResponse = setupCommandsTask.Result ?? EMPTY_SETUP_COMMANDS_RESPONSE;
+
+            return setupCommandsResponse;
+        }
+
+        public ReceiveUserCommandsResponse ReceiveCommands()
+        {
+            CommandRequest commandRequest = new CommandRequest(
+                _twitchNxClientId,
+                MESSAGE_TYPE_RECEIVE_USER_COMMANDS,
+                Array.Empty<Command>(),
+                Array.Empty<CooldownGroup>()
+            );
+
+            RestRequest restRequest = new RestRequest(string.Empty, Method.Post);
+            restRequest.AddJsonBody(commandRequest);
+
+            Task<ReceiveUserCommandsResponse?> receiveUserCommandsTask = _restClient.PostAsync<ReceiveUserCommandsResponse?>(restRequest);
+
+            if (receiveUserCommandsTask is null)
+                return EMPTY_RECEIVE_USER_COMMANDS_RESPONSE;
+
+            receiveUserCommandsTask.Wait(); // Enforce synchronous behavior
+
+            ReceiveUserCommandsResponse receiveUserCommandsResponse = receiveUserCommandsTask.Result ?? EMPTY_RECEIVE_USER_COMMANDS_RESPONSE;
+
+            return receiveUserCommandsResponse;
         }
 
     }
