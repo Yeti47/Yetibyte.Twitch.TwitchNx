277b4f5bd55dee38c27c2e0d2a68cfa81c9166ff
Alexander Herrfurth <alexander.herrfurth@outlook.de>
2022-05-30 11:54:35 +0200

    Fixed json serialization for KeyFrameMacroInstruction

--- Yetibyte.Twitch.TwitchNx.Core/CommandModel/Macros/KeyFrameMacroInstruction.cs
+++ Yetibyte.Twitch.TwitchNx.Core/CommandModel/Macros/KeyFrameMacroInstruction.cs
@@ -1,11 +1,14 @@
 ﻿namespace Yetibyte.Twitch.TwitchNx.Core.CommandModel.Macros
 {
+    [Serializable]
     public class KeyFrameMacroInstruction : IMacroInstruction
     {
+        [Serializable]
         private sealed class EmptyControllerInput : IControllerInput
         {
             public static EmptyControllerInput Instance { get;  } = new EmptyControllerInput();
 
+            [Newtonsoft.Json.JsonIgnore]
             public ControllerInputType InputType => ControllerInputType.Button;
 
             public string GetMacro() => string.Empty;
@@ -13,12 +16,16 @@
             private EmptyControllerInput() { }
         }
 
+        [Newtonsoft.Json.JsonIgnore]
         public MacroInstructionType InstructionType => MacroInstructionType.Simple;
 
+        [Newtonsoft.Json.JsonIgnore]
         public ControllerInputType ControllerInputType => ControllerInputType.Button;
 
+        [Newtonsoft.Json.JsonIgnore]
         public ControllerStick? Stick => null;
 
+        [Newtonsoft.Json.JsonIgnore]
         public ControllerButton? Button => null;
 
         public IEnumerable<TimeBoxedControllerInput> GetControllerInputs(TimeSpan parentStartTime, TimeSpan parentEndTime)
